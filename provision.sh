04b5b87ac277f104464642b751bcfca2a79ed351
Darrell Hamilton <darrell.noice@gmail.com>
2013-10-03 12:06:29 -0700

    remove puppet

---
+++ provision.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+apt-get update
+apt-get -y --fix-missing install python-pip python-virtualenv python-apt python-pycurl git
+apt-get -y remove --auto-remove puppet 
+apt-get autoclean
+apt-get clean
+apt-get purge
