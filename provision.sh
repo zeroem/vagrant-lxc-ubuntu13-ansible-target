327122ec654acc615b8d7826f472dec3239bfdee
Darrell Hamilton <darrell.noice@gmail.com>
2013-10-24 15:39:22 -0700

    additional python packages and sshd optimization

--- provision.sh
+++ provision.sh
@@ -3,8 +3,10 @@
 set -e
 
 apt-get update
-apt-get -y --fix-missing install python-pip python-virtualenv python-apt python-pycurl python-dev git 
+apt-get -y --fix-missing install python-pip python-virtualenv python-apt python-pycurl python-dev python-setuptools python-setuptools-git python-pkg-resources git 
 apt-get -y remove --auto-remove puppet 
 apt-get autoclean
 apt-get clean
 apt-get purge
+
+echo "UseDNS no" >> /etc/ssh/sshd_config
